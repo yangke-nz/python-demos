e1ef0a647204fe983197bfd1b3332656ff61fc9f
Ke Yang <ke.the.coder@gmail.com>
2023-03-26 09:46:23 +1300

    tts add mp3

--- tts/tts.py
+++ tts/tts.py
@@ -3,11 +3,13 @@ engine = pyttsx3.init()
 
 
 engine.setProperty('rate', 180)
-engine.setProperty('voice', 'com.apple.speech.synthesis.voice.tessa') 
-
+#engine.setProperty('voice', 'com.apple.speech.synthesis.voice.tessa') 
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id) 
 text = 'Pyttsx3 is a Python library for converting text to speech, and it differs from other libraries in that it functions without requiring an internet connection.'
 
 engine.say(text) 
 engine.runAndWait()
 
-#engine.save_to_file(text, 'test.wav') engine.runAndWait()
+engine.save_to_file(text, 'output_voice.mp3') 
+engine.runAndWait()
